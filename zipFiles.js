bb2c38fc253cef3a0b3f4ed9e7fa8b43464e673a
alvesa <andresistinfo@gmail.com>
2019-10-16 15:10:34 -0300

    Removendo os nomes das pastas

--- zipFiles.js
+++ zipFiles.js
@@ -1,67 +1,32 @@
 const stat = require('fs').statSync;
 const AdmZip = require('adm-zip');
 
-//const nomeProjeto = 'PCF4CustomPM025210';
+const nomeProjeto = 'nomeProjeto';
 
-//const folder = `../Projetos/BARTIRA/PCF4CustomPM025210/`;
+const folder = `pastaProjeto/`;
 
-const ziparArquivos = (nomeProjeto, folder) => {
-    const retorno = newArchive(`${nomeProjeto}.zip`, [
-        `${folder}${nomeProjeto}.sln`,
-        `${folder}.nuget`,
-        `${folder}${nomeProjeto}`
-    ],
-    folder);  
-
-    return retorno;
-}
-
-/*newArchive(`${nomeProjeto}.zip`, [
-    `${folder}PCF4CustomPM025210.sln`,
+newArchive(`${nomeProjeto}.zip`, [
+    `${folder}arquivo.sln`,
     `${folder}.nuget`,
-    `${folder}PCF4CustomPM025210`
-]);*/
+    `${folder}pastaProjeto`
+]);
 
-function newArchive(zipFileName, pathNames, folder) {
-
-    const retorno = {
-        err : true,
-        message: ''
-    }
+function newArchive(zipFileName, pathNames) {
 
     const zip = new AdmZip();
 
     pathNames.forEach(pathCompleto => {
-        const nomePathZip = pathCompleto.split('/')[pathCompleto.split('/').length-1];
+        const nomePathZip = pathCompleto.split('/')[3];
         const p = stat(pathCompleto);
         console.log(`Copiando ${nomePathZip} ...`);
-        try{
-            if (p.isFile()) {
-                console.log(p.isFile())
-                zip.addLocalFile(pathCompleto);
-            } else if (p.isDirectory()) {
-                zip.addLocalFolder(pathCompleto, nomePathZip);
-            }
-        }catch(e){
-            retorno.message = e;
-            return retorno;
-        } 
-        
+        if (p.isFile()) {
+            zip.addLocalFile(pathCompleto);
+        } else if (p.isDirectory()) {
+            zip.addLocalFolder(pathCompleto, nomePathZip);
+        }
     });
-    
-    try {
-        zip.writeZip(folder + zipFileName);   
-    } catch (e) {
-        retorno.message = e;
 
-        return retorno;
-    }
-    console.log('Arquivos zipados com sucesso. ');
-    retorno.message = 'Arquivos compactados com sucesso.';
-
-    return retorno;
+    zip.writeZip(folder + zipFileName);
+    console.log('Arquivos zipados com sucesso.');
 
 }
-
-
-module.exports = {ziparArquivos};
